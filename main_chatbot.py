04c2a99b9ada0a346f460a2c3ce0e9ae6ea0a318
AI-MeisterBin <93122637+AI-MeisterBin@users.noreply.github.com>
2022-12-11 19:50:58 +0900

    Update main_chatbot.py

--- main_chatbot.py
+++ main_chatbot.py
@@ -35,7 +35,7 @@ def get_answer(model, user_input): # 입력에 따라 답 출력
 
     data['distance'] = data['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze()) # 학습 데이터와 비교
     answer = data.loc[data['distance'].idxmax()] # 제일 유사도가 높은 값 저장
-    if answer['distance'] > 0.8: # 유사도가 0.8 이상이면 답 출력
+    if answer['distance'] > 0.7: # 유사도가 0.8 이상이면 답 출력
         return answer['챗봇']
     else: 
         return "잘 이해하지 못했어요. 좀 더 자세히 말씀 해주시겠어요?"
